88093a5a4663178de2e1d841935378f8fa3683c6
Alex Kinnane <akinnane@yahoo.co.uk>
2018-06-03 22:55:55 +0100

    Add default fonts

--- personal/custom.el
+++ personal/custom.el
@@ -15,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 85 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
  '(font-lock-function-name-face ((t (:foreground "green"))))
  '(font-lock-string-face ((t (:foreground "forest green"))))
  '(hl-line ((t (:background "gray1"))))
