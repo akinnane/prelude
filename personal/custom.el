290dc477ded1107a26716d73e731b23bbf67bcbb
Alex Kinnane <akinnane@yahoo.co.uk>
2018-06-03 22:55:33 +0100

    Change pytest settings

--- personal/custom.el
+++ personal/custom.el
@@ -8,7 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elpy-test-pytest-runner-command (quote ("pytest" "-v" "-s")))
+ '(elpy-test-pytest-runner-command (quote ("pytest")))
  '(elpy-test-runner (quote elpy-test-pytest-runner)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
